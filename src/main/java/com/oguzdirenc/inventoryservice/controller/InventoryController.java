5459ca0ca6159556f52df66be9486746315391ed
Oğuz Direnç DİNÇ <oguzdirenc@hotmail.com>
2023-10-15 17:24:07 +0300

    spring-config connected

--- src/main/java/com/oguzdirenc/inventoryservice/controller/InventoryController.java
+++ src/main/java/com/oguzdirenc/inventoryservice/controller/InventoryController.java
@@ -3,6 +3,7 @@ package com.oguzdirenc.inventoryservice.controller;
 import com.oguzdirenc.inventoryservice.dto.InventoryResponse;
 import com.oguzdirenc.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InventoryController {
 
+    @Value("${my.greeting}")
+    public String configTest;
+
     private final InventoryService inventoryService;
 
     @GetMapping
@@ -20,4 +24,12 @@ public class InventoryController {
     public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping("/config")
+    public String testConfig(){
+        return configTest;
+    }
+
 }
